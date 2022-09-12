516617b70e3a7418672cb7d6daa5a8ba1c01bffb
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-12 20:28:47 -0300

    Resolução, somas das letras.

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,15 @@
-const letras = ["A", "a", "B", "C", "E", "e"];
+const letras = ["A", "a", "B", "C", "f", "a"];
+
+let somaLetras = 0
+
+for (let letra of letras) {
+    if (letra === "e" || letra === "E") {
+        somaLetras++;
+    }
+}
+if (somaLetras > 0) {
+    console.log(`Foram encontradas ${somaLetras} "e" ou "E".`);
+
+} else {
+    console.log(`Nao foram encontradas letras "e" ou "E".`)
+}
