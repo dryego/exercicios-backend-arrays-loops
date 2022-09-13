cc9816d48e79d0689da805f411a801bf3a9419a7
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-12 22:18:14 -0300

    Resolução, soma dos pares.

--- 06/index.js
+++ 06/index.js
@@ -1,2 +1,11 @@
-const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+const numeros = [3, 4, 7, 8, 1, 6, 5, 10, 10];
+
+let somaPar = 0;
+
+for (let par of numeros) {
+    if ((par % 2) == 0) {
+        somaPar += par;
+    }
+}
+console.log(somaPar);
 
