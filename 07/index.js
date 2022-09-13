36b41a2b472616fab40fb6da03a2936a4d277155
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-12 22:32:37 -0300

    Resolução, nomes com 'a' ou 'A'.

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,10 @@
 const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+
+let nomesA = [];
+
+for (let nome of nomes) {
+    if (nome[0] === "a" || nome[0] === "A") {
+        nomesA.push(nome);
+    }
+}
+console.log(nomesA);
