742398b9b0ca7a2510149e896b74cbd67d9974e5
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-14 09:06:06 -0300

    Resolução, numeros entre 10 e 20 e > 100.

--- 11/index.js
+++ 11/index.js
@@ -1,2 +1,12 @@
 const original = [5, 7, 13, 17, 26, 34, 118, 245];
 
+let selecao = []; //Guarda valores entre 10 e 20 e > que 100;
+
+for (let maiores of original) {
+    if (maiores >= 10 && maiores <= 20) {
+        selecao.push(maiores);
+    } else if (maiores > 100) {
+        selecao.push(maiores);
+    }
+}
+console.log(selecao);
