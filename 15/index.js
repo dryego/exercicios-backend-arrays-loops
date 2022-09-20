e0fd4b394e7323d8d8aed4d7c33b1988b04c9843
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-20 19:34:18 -0300

    Resolução, pares e impares.

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,13 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+for (let busca of original) {
+    if ((busca % 2) == 0) {
+        pares.push(busca);
+    } else {
+        impares.push(busca);
+    }
+}
+console.log(pares);
+console.log(impares);
+
